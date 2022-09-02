85ba25d0bce5e8f39c0383bd94fcc154bd7023df
reyno405 <reyno405@gmail.com>
2022-09-02 14:38:58 -0400

    this update has api used for popular movies

--- assests/java.js
+++ assests/java.js
@@ -8,7 +8,7 @@ getMovies(API_URL);
 function getMovies(url) {
     
     fetch(url).then(res => res.json()).then(data => {
-        
+        console.log(data.results)
         showMovies(data.results);
     }) 
 
@@ -16,23 +16,36 @@ function getMovies(url) {
 
 function showMovies(data) {
 
-    data.forEach(mpvie => {
+    data.forEach(movie => {
+        const {title, poster_path, vote_average, overview} = movie;
         const movieEl = document.createElement('div');
         movieEl.classList.add('movie')
         movieEl.innerHTML = `
         <div class="movie">
-            <img src="" alt="Image" >
+            <img src="${IMG_URL+poster_path}" alt="${title}" >
 
             <div class="movie-info">
-                <h3>Movie Title</h3>
-                <span class="green">9.8</span>
+                <h3>"${title}</h3>
+                <span class="${getColor(vote_average)}">${vote_average}</span>
             </div>
 
             <div class="overview">
-                <h3>Overview</h3>
                 
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab eligendi ipsa temporibus rem possimus ea mollitia? Voluptatem non ad odio.
+            <h3>Overview</h3>
+            ${overview};
+
             </div>
         `
     })
+}
+
+function getColor(vote){
+    if(vote >= 8) {
+        return 'green'
+    }else if (vote >= 5){
+        return "orange"
+    }else {
+        return 'red'
+    }
+
 }
