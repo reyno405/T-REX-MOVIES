4df809902cc8d508d8382140479a2268536dbf42
reyno405 <reyno405@gmail.com>
2022-09-02 14:43:03 -0400

    in this update there will be working images pulled form api for popular movies

--- assests/java.js
+++ assests/java.js
@@ -1,28 +1,31 @@
 const API_KEY = 'api_key=96db98bb71a271cc952cb07d8c7a019f';
 const BASE_URL = 'https://api.themoviedb.org/3';
-const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' +API_KEY;
+const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+
+const main = document.getElementById('main')
 
 getMovies(API_URL);
 
 function getMovies(url) {
-    
+
     fetch(url).then(res => res.json()).then(data => {
         console.log(data.results)
         showMovies(data.results);
-    }) 
+    })
 
 }
 
 function showMovies(data) {
+    main.innerHTML = '';
 
     data.forEach(movie => {
-        const {title, poster_path, vote_average, overview} = movie;
+        const { title, poster_path, vote_average, overview } = movie;
         const movieEl = document.createElement('div');
         movieEl.classList.add('movie')
         movieEl.innerHTML = `
         <div class="movie">
-            <img src="${IMG_URL+poster_path}" alt="${title}" >
+            <img src="${IMG_URL + poster_path}" alt="${title}" >
 
             <div class="movie-info">
                 <h3>"${title}</h3>
@@ -36,15 +39,17 @@ function showMovies(data) {
 
             </div>
         `
+
+        main.appendChild(movieEl);
     })
 }
 
-function getColor(vote){
-    if(vote >= 8) {
+function getColor(vote) {
+    if (vote >= 8) {
         return 'green'
-    }else if (vote >= 5){
+    } else if (vote >= 5) {
         return "orange"
-    }else {
+    } else {
         return 'red'
     }
 
