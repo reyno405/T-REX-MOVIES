d9800cc7834153eb873a5fd99102203b087247a6
reyno405 <reyno405@gmail.com>
2022-09-02 20:55:22 -0400

    added a jumbotron with the title

--- assests/java.js
+++ assests/java.js
@@ -2,11 +2,12 @@ const API_KEY = 'api_key=96db98bb71a271cc952cb07d8c7a019f';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
-const searchURL = BASE_URL + '/search/movie?'+API_KEY;
+const searchURL = BASE_URL + '/search/movie?' + API_KEY;
 
 const main = document.getElementById('main');
 const form = document.getElementById('form');
 const search = document.getElementById('search');
+
 getMovies(API_URL);
 
 function getMovies(url) {
@@ -60,8 +61,10 @@ function getColor(vote) {
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     const searchTerm = search.value;
-   
+
     if (searchTerm) {
-        getMovies(searchURL+'&query='+searchTerm)
+        getMovies(searchURL + '&query=' + searchTerm)
+    } else {
+        getMovies(API_URL);
     }
 })
